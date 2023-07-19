abd133b5cd575e0a50deee7970dd0ae228dd1f44
Jimit Patel <jimitpatel9879@gmail.com>
2023-07-19 22:15:08 +0530

    .

---
+++ CalculatorTestingXUnit/CalculatorTesting.cs
@@ -0,0 +1,16 @@
+﻿using CalculatorforXUnit;
+using Xunit;
+
+namespace CalculatorTestingXUnit
+{
+    public class CalculatorTesting
+    {
+        [Fact]
+        public void TestAdditionWithInValidValue()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Addition(12, 13);
+            Assert.Equal(25, result);
+        }
+    }
+}
---
+++ CalculatorforXUnit/Calculator.cs
@@ -0,0 +1,8 @@
+﻿namespace CalculatorforXUnit
+{
+    public class Calculator
+    {
+        public int Addition(int first , int Second)
+        { return first + Second; }
+    }
+}
