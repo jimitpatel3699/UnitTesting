a70c89a2f6756315892c51b57307b48d9d206e60
Jimit Patel <jimitpatel9879@gmail.com>
2023-07-19 22:33:25 +0530

    XUnit Testing

--- CalculatorforXUnit/Calculator.cs
+++ CalculatorforXUnit/Calculator.cs
@@ -1,8 +1,31 @@
 ﻿namespace CalculatorforXUnit
 {
-    public class Calculator
+    public class Calculator :IDisposable
     {
-        public int Addition(int first , int Second)
-        { return first + Second; }
+        public int Addition(int first, int second)
+        {
+            return first + second;
+        }
+        public int Subtraction(int first, int second)
+        {
+            return second - first;
+        }
+        public int Multiplication(int first, int second)
+        {
+            return first * second;
+        }
+        public float Divide(float first, float second)
+        {
+            if (second == 0)
+            {
+                throw new ArgumentException($"Divided by zero not possible");
+            }
+            return first / second;
+        }
+
+        public void Dispose()
+        {
+
+        }
     }
 }
