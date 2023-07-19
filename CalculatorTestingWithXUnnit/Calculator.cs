7da437303025c654c706081d64ffc050a724066a
Jimit Patel <jimitpatel9879@gmail.com>
2023-07-19 15:45:59 +0530

    intial classes

---
+++ CalculatorTestingWithXUnnit/Calculator.cs
@@ -0,0 +1,27 @@
+﻿namespace CalculatorTestingWithXUnnit
+{
+    public class Calculator
+    {
+      public int Addition(int first, int second)
+        {
+            return first + second;
+        }
+        public int Subtraction(int first, int second) 
+        {   
+            return second - first;
+        }
+        public int Multiplication(int first, int second) 
+        {
+            return (first *  second);
+        }
+        public int division(int first, int second) 
+        {
+            if(second == 0)
+            {
+                throw new ArgumentException("Divide by 0 not possible");
+            }
+            return first/second;
+        }
+
+    }
+}
---
+++ CalculatorXunit.Test/CalculatorNUniTest.cs
@@ -0,0 +1,21 @@
+﻿using CalculatorTestingWithXUnnit;
+using Xunit;
+
+namespace CalculatorXunit.Test
+{
+    public class CalculatorNUniTest
+    {
+        [Fact]
+        public void AddtionWithValidValues()
+        {
+            var calc = new Calculator();
+            var actual = calc.Addition(2, 3);
+            Assert.Equal(5, actual);
+        }
+        [Fact]
+        public void SubtarctionWithValidValues()
+        {
+
+        }
+    }
+}
